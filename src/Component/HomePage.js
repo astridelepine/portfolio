f7f7959c38d69402165c66d9bfef7057a2820527
astridelepine <delas2924@gmail.com>
2024-06-19 23:10:09 +0200

    testColor

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -31,7 +31,7 @@ const HomePage = () => {
             </Flex>
             <Text fontSize={"6xl"} color={"white"} fontWeight={"bold"} fontFamily={"Playwrite"} width={500} ml={80} mt={200} className='slide'>Bienvenue sur mon Portfolio </Text>
             <Flex width={"100%"} justifyContent={"flex-end"} pr={200}>
-              <Text color={"white"} width={400} textAlign={"left"} border={"dashed 2px"} height={"fit-content"} padding={"20px"} borderRadius={"10px"}>Saluuut, Je m'appelle Astrid et je suis étudiante en BUT informatique. Dans ce portfolio je vous fais part de mes projets et compétence informatique mais aussi de mes autres centres d'interêt !</Text>
+              <Text color={"#ffffff"} width={400} textAlign={"left"} border={"dashed 2px"} height={"fit-content"} padding={"20px"} borderRadius={"10px"}>Saluuut, Je m'appelle Astrid et je suis étudiante en BUT informatique. Dans ce portfolio je vous fais part de mes projets et compétence informatique mais aussi de mes autres centres d'interêt !</Text>
             </Flex>
             <Box width={"100%"}>
               <Fleurs/>
