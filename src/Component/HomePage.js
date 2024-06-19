41bc1e77a024084a3a943fcd15ff38e381692bc5
astridelepine <delas2924@gmail.com>
2024-06-19 23:06:57 +0200

    couleur

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -20,18 +20,18 @@ const HomePage = () => {
         <Grid minH="100vh" p={3} >
             <Flex fontFamily={"Playwrite"} justifyContent={"flex-end"} zIndex={20}>
               <Link to={"/projets"}>
-                <Text pr={4} _hover={{color:"#f5ffc5", fontWeight:'bold'}}>projets</Text>
+                <Text pr={4} color={"white"} _hover={{color:"#f5ffc5", fontWeight:'bold'}}>projets</Text>
               </Link>
               <Link to={"/competences"}>
-                <Text pr={4} _hover={{color:"#c7ffc5", fontWeight:'bold'}} >competences</Text>
+                <Text pr={4} color={"white"} _hover={{color:"#c7ffc5", fontWeight:'bold'}} >competences</Text>
               </Link>
               <Link to={"/passions"}>
-                <Text pr={4} _hover={{color:"#c5c9ff", fontWeight:'bold'}} >passions</Text>
+                <Text pr={4} color={"white"} _hover={{color:"#c5c9ff", fontWeight:'bold'}} >passions</Text>
               </Link>
             </Flex>
-            <Text fontSize={"6xl"} fontWeight={"bold"} fontFamily={"Playwrite"} width={500} ml={80} mt={200} className='slide'>Bienvenue sur mon Portfolio </Text>
+            <Text fontSize={"6xl"} color={"white"} fontWeight={"bold"} fontFamily={"Playwrite"} width={500} ml={80} mt={200} className='slide'>Bienvenue sur mon Portfolio </Text>
             <Flex width={"100%"} justifyContent={"flex-end"} pr={200}>
-              <Text width={400} textAlign={"left"} border={"dashed 2px"} height={"fit-content"} padding={"20px"} borderRadius={"10px"}>Saluuut, Je m'appelle Astrid et je suis étudiante en BUT informatique. Dans ce portfolio je vous fais part de mes projets et compétence informatique mais aussi de mes autres centres d'interêt !</Text>
+              <Text color={"white"} width={400} textAlign={"left"} border={"dashed 2px"} height={"fit-content"} padding={"20px"} borderRadius={"10px"}>Saluuut, Je m'appelle Astrid et je suis étudiante en BUT informatique. Dans ce portfolio je vous fais part de mes projets et compétence informatique mais aussi de mes autres centres d'interêt !</Text>
             </Flex>
             <Box width={"100%"}>
               <Fleurs/>
